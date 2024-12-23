c762f220f79de2490da85045f74c50c6cb3e42a6
harmonia2side <breno_odnan@hotmail.com>
2024-12-23 17:52:18 -0300

    lab1 - progresso na 8.3

--- lab1/8.3/main.c
+++ lab1/8.3/main.c
@@ -1,9 +1,67 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+//#include <stdlib.h>
 
-int main(){
-    int vetor[10] = {47,3,0,7,947,9,8,66,64,16};
-    printf("%d", vetor[0]);
-    
-    return 0;
+void quickSort(int valor[], int esquerda, int direita);
+
+int main() {
+  int vetor[10] = {47, 3, 0, 7, 947, 9, 8, 66, 64, 16};
+//   printf("%d", vetor[0]);
+
+  int is_parent = fork();
+  if (is_parent) {
+    return 1;
+    // do normal order
+    clock_t c1, c2; /* variaveis que contam ciclos de processador */
+    double tmp;
+    c1 = clock();
+    // ... codigo a ser executado
+    c2 = clock();
+    tmp = (c2 - c1) * 1000 / CLOCKS_PER_SEC; // tempo de execucao em milisec .
+  } else {
+    // do quicksort
+    clock_t c1, c2; /* variaveis que contam ciclos de processador */
+    double tmp;
+    c1 = clock();
+    quickSort(vetor, vetor[0], vetor[1]);
+    c2 = clock();
+    tmp = (c2 - c1) * 1000 / CLOCKS_PER_SEC; // tempo de execucao em milisec .
+  }
+  int n;
+  printf("\nvetor:\n");
+  for(n=0;n<10;n++){
+    printf("%d, ", vetor[n]);
+  }
+  printf("\n");
+
+  return 0;
+}
+
+void quickSort(int valor[], int esquerda, int direita) {
+  int i, j, x, y;
+  i = esquerda;
+  j = direita;
+  x = valor[(esquerda + direita) / 2];
+  while (i <= j) {
+    while (valor[i] < x && i < direita) {
+      i++;
+    }
+    while (valor[j] > x && j > esquerda) {
+      j--;
+    }
+    if (i <= j) {
+      y = valor[i];
+      valor[i] = valor[j];
+      valor[j] = y;
+      i++;
+      j--;
+    }
+  }
+  if (j > esquerda) {
+    quickSort(valor, esquerda, j);
+  }
+  if (i < direita) {
+    quickSort(valor, i, direita);
+  }
 }
