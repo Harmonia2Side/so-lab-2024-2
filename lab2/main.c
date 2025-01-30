8ef3fbb828654dc24e9bf4e2e40e73e416720bb9
Breno Souza <breno_odnan@hotmail.com>
2025-01-30 12:19:45 -0300

    lab2q5: finaliza questão

--- lab2/main.c
+++ lab2/main.c
@@ -1,15 +1,36 @@
+#include <errno.h>
 #include <stdio.h>
-#include <unistd.h> 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-int main(){
-     if(fork() == 0){
-        execl( "/bin/ls","ls","-l", NULL) ;
-     }
- 
-     else{ 
-        sleep(2) ;
-        /* espera o fim de ls para executar o printf() */ 
-        printf ("Eu sou o pai e finalmente posso continuar\n") ;
-    } 
-    return 1;
+pid_t r_wait(int *status) {
+	int retval;
+	while (((retval = wait(status)) == -1) && (errno == EINTR)) ;
+	return retval;
 }
+int main(int argc, char *argv[]) {
+   // printf("argv = %s\n", argv[0]);
+	pid_t childpid;
+	if ( argc < 2 ) {
+		fprintf (stderr, "Uso: %s comando arg1 arg2 ... \n", argv[0]);
+		return 1;
+	}
+	childpid = fork();
+	if ( childpid == -1 ) {
+		perror ("Falha em fork");
+		return 1;
+	}
+	if (childpid == 0 ) {	/* Código filho */
+		execvp(argv[1], &argv[1]);
+		perror ("Filho falhou em execvp o comando");
+		return 1;
+	}
+	if (childpid != r_wait(NULL)) {	/* Código Pai */
+		perror ("Parent failed to wait");
+		return 1;
+	}
+	printf("Filho completo -- pai saindo imediatamente.\n");
+	return 0;
+}
+
