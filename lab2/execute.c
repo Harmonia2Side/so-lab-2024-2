34fae3180dfda5ad85c98cd2f2c179a613dc23dd
harmonia2side <breno_odnan@hotmail.com>
2025-01-29 23:44:56 -0300

    lab2: q'5': adiciona código rascunho

---
+++ lab2/execute.c
@@ -0,0 +1,1 @@
+#include <stdio.h>#include <unistd.h> int main(){ if ( fork()==0 ) execl( "/bin/ls","ls","-l", NULL) ; else { sleep(2) ; /* espera o fim de ls para executar o printf() */ printf ("Eu sou o pai e finalmente posso continuar\n") ;} return 1;}
