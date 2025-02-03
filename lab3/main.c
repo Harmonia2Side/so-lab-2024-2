41084349a60a5380c9a16bbc008373ef7272e81b
Breno Souza <breno_odnan@hotmail.com>
2025-02-03 18:33:21 -0300

    lab3q3: adiciona comentários explicativos

--- lab3/main.c
+++ lab3/main.c
@@ -28,19 +28,40 @@ int main() {
 		kill(getppid(), SIGUSR1);
 		exit(0);
 	}
+	// Inicializa a máscara sem nenhum sinal (nenhum sinal bloqueado)
 	sigemptyset(&sact.sa_mask);
+	// Não configura nenhuma flag especial
 	sact.sa_flags = 0;
+	// Define função de tratamento do sinal
 	sact.sa_handler = catcher;
+	/* 
+		As linhas a seguir, que executam a função sigaction, associam os sinais 
+		SIGUSR1 e SIGUSR2 à estrutura sact definida anteriormente.
+		Na estrutura estão contidas:
+		- sa_mask: a máscara de sinais bloqueados,
+		- sa_flags: flag especial (não relevante no caso)
+		- sa_handler: função de tratamento do sinal
+		Sem a utilização do sigaction, o Sistema não associa os sinais especificados com a
+		estrutura de "configuração", e então a função de tratamento não seria chamada e os sinais
+		não seriam ignorados. 
+		Por ser um ponteiro, modificações posteriores a 'sact' resultarão em mudanças efetivas
+		de "configuração".
+	*/
+	// "configura" SIGUSR1
 	if (sigaction(SIGUSR1, &sact, NULL) != 0)
 		perror("1st sigaction() error");
+	// "configura" SIGUSR2
 	else if (sigaction(SIGUSR2, &sact, NULL) != 0)
 		perror("2nd sigaction() error");
 	else {
+		// preenche a mascará com todos os sinais suportados no sistema
 		sigfillset(&sigset);
+		// Remove bloqueio de SIGUSR1
 		sigdelset(&sigset, SIGUSR1);
 		time(&t);
 		printf("parent is waiting for child to send SIGUSR1 at %s",
 		ctime(&t));
+		// aguarda envio de algum sinal
 		if (sigsuspend(&sigset) == -1) {
 			perror("sigsuspend() returned -1 as expected");
 		}
